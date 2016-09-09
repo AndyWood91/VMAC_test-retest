d46575ef15926ab87c7945482be972a32110dade
Andy Wood <a.wood@unsw.edu.au>
2016-09-09 15:54:36 +1000

    edits to RSVP but it's not working properly as yet

--- Program/VMAC_test_retest.m
+++ Program/VMAC_test_retest.m
@@ -8,10 +8,10 @@ KbName('UnifyKeyNames');
 global DATA  % needs to be in invoking program/function too
 
 
-% participant_details({1:4}, 2, true);
+participant_details({1:4}, 2, true);
 
 
-spatial_test_retest;
+% spatial_test_retest;
 
 
 RSVP_test_retest;
