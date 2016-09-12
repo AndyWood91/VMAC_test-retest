812b3a0c1472da798b7076209238524044db2da4
Andy Wood <a.wood@unsw.edu.au>
2016-09-12 10:49:20 +1000

    creating test version

--- Program/VMAC_test_retest.m
+++ Program/VMAC_test_retest.m
@@ -1,11 +1,15 @@
 clear all;
 sca;
 clc;
-KbName('UnifyKeyNames');
+KbName('UnifyKeyNames');  % standardise across OSs
 
 
 % variable declarations
 global DATA  % needs to be in invoking program/function too
+global testing
+
+% testing = 0;  % experimental version
+testing = 1;  % testing version
 
 
 participant_details({1:4}, 2, true);
