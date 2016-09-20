7895df3421d7cce933d4716b6e5cb826b66151f5
Andrew Wood <awoo5332@gmail.com>
2016-09-20 14:30:31 +1000

    playing around with the screen settings, can't find the spatial display bug

--- Program/MainScreen.m
+++ Program/MainScreen.m
@@ -2,9 +2,7 @@ classdef MainScreen
     
     properties
         screen_number = 0;
-        main_window = Screen('OpenWindow', 0, [0 0 0]);
-        
-    
+        main_window = Screen('OpenWindow', 0, [0 0 0]);  
     end
     
     methods
